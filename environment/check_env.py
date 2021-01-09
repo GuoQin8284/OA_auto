d84b0b7b9cf0540ec61fb980d684cc7acbd051aa
guoqin <1143908462@qq.com>
2021-01-09 20:55:31 +0800

    tianjia huanjin

--- environment/check_env.py
+++ environment/check_env.py
@@ -10,25 +10,30 @@ def read_package(pathname="python_package"):
         data = f.readlines(-1)
         data_list = [x.replace("\n","").split("==") for x in data]
         data_dict = [{"{}".format(x[0]): "{}".format(x[-1])} for x in data_list]
-        print(data_dict)
+        # print(data_dict)
         return data_dict
 
 def checkPyEnv():
-    # env_list = os.system("pip list > echo")
+    print("正在检查运行环境...")
     env_project = os.popen("pip list")
     env_list = env_project.read().splitlines()
     set_env = read_package()
-    print("env_list",env_list)
     temp_cur_envName = [x.split(" ")[0] for x in env_list]
-    print("temp_cur_envName:",temp_cur_envName)
     for set_env_key in set_env:
         key = list(set_env_key.keys())[0]
-        value = set_env_key["key"]
-        print("set_env_key.keys():",key)
-        if key in  temp_cur_envName:
-            pass
-        else:
-            os.system("pip install {}=={}".format(key,value))
+        value = set_env_key[key]
+        try:
+            if key in  temp_cur_envName:
+                print("{} 已安装.".format(key))
+            else:
+                print("{} 未安装，正在安装...".format(key))
+                os.system("pip install {}=={}".format(key,value))
+                print("{} 已安装完成.".format(key))
+
+        except Exception as f:
+            print("运行环境检查失败...")
+            raise f
+    print("运行环境检查完成！")
 
 if __name__ == "__main__":
     checkPyEnv()
--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ import time
 
 
 # 启动pytest
+from environment.check_env import checkPyEnv
+
+
 def setup_pytest():
+    checkPyEnv() # 检查运行环境
     start_time = time.strftime("%Y-%m-%d %H:%M:%S")
     print("{}启动pytest...".format(start_time))
     pytest = "pytest"
     allure_report = r"allure generate report/data -o report/html --clean"
-    os.system(pytest)
-    os.system(allure_report)
+    os.system(pytest)  # 启动pytest
+    os.system(allure_report)  # 生成allure测试报告
     end_time = time.strftime("%Y-%m-%d %H:%M:%S")
     print("{} pytest执行完成!".format(end_time))
 
